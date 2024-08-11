aa8c2108edded34ce6d147a37029749548ba2844
Maksym <max.borovique@gmail.com>
2024-08-11 16:49:12 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const formInputs = document.querySelectorAll("form input");
+formInputs.forEach((input)=>{
+    const inputLabel = document.createElement("label");
+    inputLabel.classList.add("field-label");
+    inputLabel.setAttribute("for", input.id);
+    const labelText = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+    inputLabel.textContent = labelText;
+    input.setAttribute("placeholder", labelText);
+    input.parentNode.insertBefore(inputLabel, input);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
